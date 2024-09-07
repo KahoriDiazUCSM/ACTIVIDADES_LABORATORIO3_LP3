921755fa3a0dc75bb1ef25bf00747e816c888735
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-09-06 22:53:54 -0500

    Update Arco.java

--- ACTIVIDADES/ACTIVIDAD3/Arco.java
+++ ACTIVIDADES/ACTIVIDAD3/Arco.java
@@ -1,6 +1,6 @@
 public class Arco implements TipoDeArma {
     @Override
     public int calcularDaño(int baseDamage, int level) {
-        return baseDamage + (level * 3);  -> Cálculo de daño específico para el arco
+        return baseDamage + (level * 3);
     }
 }
